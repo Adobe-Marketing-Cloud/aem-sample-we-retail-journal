5c1d2fbf8bf4bf4a672364db19cb2969d0c08736
Gilles Knobloch <gknob@adobe.com>
2018-10-09 10:23:05 +0200

    @trivial - Repository Moved to https://github.com/adobe/aem-sample-we-retail-journal

--- angular-app/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  API_HOST: "",
-  APP_ROOT_PATH: "/content/we-retail-journal/angular",
-  CONTEXT_PATH: "" // configuration style: contextpath/
-};
